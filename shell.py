f2d5fadf2ddf418d7af561b1e259d2d8c484809d
byt3Buster <byt3buster@gmail.com>
2025-11-15 06:17:23 +0100

    refactor(shell): ajout du parser, interpreter

--- shell.py
+++ shell.py
@@ -1,5 +1,6 @@
 from lexer import Lexer
-
+from parser import Parser
+from interpreter import Interpreter
 """
 Shell interactif pour les tests
 """
@@ -7,10 +8,18 @@ Shell interactif pour les tests
 while True:
     """
     > 1 + 1
-    [1, +, 1]
+    Tokens -> [1, +, 1] 
+    BTree -> [1, +, 1]
+    Result -> 2
     """
     exp = input("> ")
     tokenizer = Lexer(exp)
     tokens = tokenizer.tokenize()
 
-    print(tokens)
+    parser = Parser(tokens)
+    tree = parser.parse()
+
+    interpreter = Interpreter(tree)
+    result = interpreter.interpret()
+
+    print(f"Tokens -> {tokens} \nBTree -> {tree}\nResult -> {result}")
